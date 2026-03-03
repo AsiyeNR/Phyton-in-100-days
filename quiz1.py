70c4bc3ab27d3f3f62e1b4a9ad1025409f8f411d
Asiye Nur <166254553+AsiyeNR@users.noreply.github.com>
2026-03-03 23:26:24 +0300

    Fix variable names and add answers in quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -1,31 +1,59 @@
 # 1) Asagidaki string'in 5. harfini bir degiskene atayiniz
 my_string = "Python Ogreniyorum"
-# cevap
+# cevap 
+my_letter = mu_string[4]
+
 # 2) Asagidaki String'in 5. ve 8. karakteri arasindaki tum harflerini yazdiriniz (5 ve 8 dahil)
 my_new_string = "ProgramlamayaMerhabaDedik"
 #cevap
+my_new_string[4:8:]
+
 # 3) Asagidaki String'i kod ile tersten yazin
 my_last_string = "Afyonkarahisarlilastiramadiklarimizdanmisiniz"
 #cevap
+my_last_string[::-1]
+
 # 4) Asagidaki islemin sonucu hangi veri tipinde olacaktir?
 4 + 12.2 + 48
 #cevap
+type(4+12.2+48)
+float
+
 # 5) Asagidaki islemin sonucu kactir?
 5 + 7 * 12
 #cevap
+89
+
 # 6) Bu listeyi en az 2 farkli yoldan olusturunuz: [1,3,"a"]
 #cevap
+my_list = [1,3,"a"]
+my_list = list()
+my_list.append(1)
+my_list.append(3)
+my_list.append("a")
+
 # 7) Asagidaki "b"'yi tek satirda aliniz:
 my_list = [3.14,4,[2,3,"b"],True]
 #cevap
+my_list[2][2]
+
 # 8) Asagidaki "a"'yi tek satirda aliniz:
 my_dictionary = {"key1":20.25, "kk2":[40,{"k21":"a"}]}
 #cevap
+my_dictionary[kk2][1][k21]
+
 # 9) Asagidaki liste set'e cevirilince hangi degerler icinde kalacaktir?
 my_list_to_be_set = [3,4,9,3,21,22,4,3,9,10,21,22]
 #cevap
+set(my_list_to_be_set)
+{3,4,9,10,21,22}
+
 # 10) Asagidaki ifadenin sonucu ne olacaktir?
 x = 30 * 5 + 3
 y = 108 - 2 * 4
 x > y
 #cevap
+x = 153
+y = 100
+x > y
+
